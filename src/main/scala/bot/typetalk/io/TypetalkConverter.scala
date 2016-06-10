2f037334044910cc3f46059a14c6b32920d05702
nasbsil14 <nasbsil14@yahoo.co.jp>
2016-06-10 20:12:09 +0900

    update

--- src/main/scala/bot/typetalk/io/TypetalkConverter.scala
+++ src/main/scala/bot/typetalk/io/TypetalkConverter.scala
@@ -9,8 +9,10 @@ import bot.typetalk.io.json._
 import com.typesafe.config.Config
 import spray.json._
 
-import scala.concurrent.Await
+import scala.concurrent.{Await, Future, ExecutionContextExecutor}
 import scala.concurrent.duration.Duration
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait TypetalkConverter extends SprayJsonSupport with DefaultJsonProtocol {
   def config: Config
@@ -18,64 +20,10 @@ trait TypetalkConverter extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val nttAPIRequestFormatter = jsonFormat14(NTTAPIRequest)
   implicit val nttAPIResponseFormatter = jsonFormat5(NTTAPIResponse)
 
-  implicit val typetalkTopicUnmarshaller: FromEntityUnmarshaller[TopicEntity] = {
-    PredefinedFromEntityUnmarshallers.defaultUrlEncodedFormDataUnmarshaller.map { data: FormData =>
-      println(data)
-      TopicEntity(
-        data.fields.getOrElse("id","")
-        , data.fields.getOrElse("name","")
-        , data.fields.getOrElse("suggestion","")
-        , data.fields.getOrElse("lastPostedAt","")
-        , data.fields.getOrElse("createdAt","")
-        , data.fields.getOrElse("updatedAt","")
-      )
-    }
-  }
+  implicit val typetalkTopicFormatter = jsonFormat6(TopicEntity)
+  implicit val typetalkAccountFormatter = jsonFormat7(AccountEntity)
+  implicit val typetalkPostFormatter = jsonFormat12(PostEntyity)
 
-  implicit val typetalkAccountUnmarshaller: FromEntityUnmarshaller[AccountEntity] = {
-    PredefinedFromEntityUnmarshallers.defaultUrlEncodedFormDataUnmarshaller.map { data: FormData =>
-      println(data)
-      AccountEntity(
-        data.fields.getOrElse("id","")
-        , data.fields.getOrElse("name","")
-        , data.fields.getOrElse("fullName","")
-        , data.fields.getOrElse("suggestion","")
-        , data.fields.getOrElse("imageUrl","")
-        , data.fields.getOrElse("createdAt","")
-        , data.fields.getOrElse("updatedAt","")
-      )
-    }
-  }
-
-  implicit val typetalkPostUnmarshaller: FromEntityUnmarshaller[PostEntyity] = {
-    PredefinedFromEntityUnmarshallers.defaultUrlEncodedFormDataUnmarshaller.map { data: FormData =>
-      println(data)
-      PostEntyity(
-        data.fields.getOrElse("id","")
-        , data.fields.getOrElse("topicId","")
-        , data.fields.getOrElse("replyTo","")
-        , data.fields.getOrElse("message","")
-        , Await.result(Unmarshal(data.fields.getOrElse("account", "")).to[AccountEntity], Duration.Inf)
-        , data.fields.getOrElse("mention","")
-        , data.fields.getOrElse("attachments","")
-        , data.fields.getOrElse("likes","")
-        , data.fields.getOrElse("talks","")
-        , data.fields.getOrElse("links","")
-        , data.fields.getOrElse("createdAt","")
-        , data.fields.getOrElse("updatedAt","")
-      )
-    }
-  }
-
-  implicit val typetalkWebHookRequestUnmarshaller: FromEntityUnmarshaller[TypetalkWebhookRequest] = {
-    PredefinedFromEntityUnmarshallers.defaultUrlEncodedFormDataUnmarshaller.map { data: FormData =>
-      println(data)
-      TypetalkWebhookRequest(
-        Await.result(Unmarshal(data.fields.getOrElse("topic", "")).to[TopicEntity], Duration.Inf)
-        , Await.result(Unmarshal(data.fields.getOrElse("post", "")).to[PostEntyity], Duration.Inf)
-      )
-    }
-  }
-
+  implicit val typetalkWebhookRquestFormatter = jsonFormat2(TypetalkWebhookRequest)
   implicit val typetalkWebhookResponseFormatter = jsonFormat2(TypetalkWebhookResponse)
 }
